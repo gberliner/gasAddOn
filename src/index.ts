a5e821cf35dc9ff99cb96dd0f584057252650c22
gberliner <guy.berliner@papercut.com>
2019-02-26 13:54:35 -0800

    remove comments

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
 import { doGet } from "./server/webapp";
 import "./es6";
-//import global from "./global";
-//import {global} from "gas-entry-generator"
-//global defined by gas-entry-generator
-/// <reference path = "globals.d.ts" />
 
+/// <reference path = "globals.d.ts" />
 global.doGet = doGet;
 
 global.sendmail = (email = "amit@labnol.org") => {
